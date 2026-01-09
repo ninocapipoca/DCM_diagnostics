7fd58baa2c436f72ab8fd5a79015cb7601e386a3
Nina <cninamatos@pm.me>
2026-01-09 15:51:08 +0100

    Removes SVA code from ExploratoryAnalysis file and adds cleaned data export

--- DataPreparationExploratoryAnalysis.R
+++ DataPreparationExploratoryAnalysis.R
@@ -6,6 +6,8 @@
 # Start date: 08/01/2026      									                              #
 #=============================================================================#
 
+# NOTE - may require revision if batch correction needs to be implemented
+
 # Load packages ------------------------------------------------
 library(ggplot2)
 library(tidyr)
@@ -20,6 +22,7 @@ library(clusterProfiler)
 library(org.Hs.eg.db)
 library(pathview)
 library(biomaRt)
+
 #-----------------------------------------------------------------------------#
 # Data import & participant table
 #-----------------------------------------------------------------------------#
@@ -43,41 +46,9 @@ gxData <- gxData_all[, colnames(gxData_all) %in% rownames(metadata)]
 # Turn all character-type columns into factors (categorical variables)
 metadata <- metadata |> mutate_if(is.character, as.factor)
 
-# TODO - Any data clean-up? Check code below in meeting :) 
-
-# SVA -------------------------------------------------------------------------
-# NOTE - The results are very peculiar and I don't know if it's done correclty.. I will ask at next Q&A
-
-# Check gxData and metadata samples are in the same order
-all(colnames(gxData) == rownames(metadata))
-
-# Create a Model with the variable of interest (etiology, whose variation we want to explain in our analysis)
-model <- model.matrix(~ etiology, data = metadata)
-
-# Create a Null model (baseline) without a variable of interest
-model0 <- model.matrix(~ 1, data = metadata)  # Explains overall avg expression
-
-# Create a surrogate variable (unknown factors; separates known variation from total variation and compares to baseline)
-# Will be a matrix with rows = samples, cols = unknown factors (+ value per sample) that cause variability in the data
-sv.obj <- sva(as.matrix(gxData), model, model0)
-modelSV <- cbind(model, sv.obj$sv) # NOTE - the number of SVs is very high, so we check what's up in the next lines
-
-# Check which surrogate variables explain the most variance
-sv.vars <- apply(sv.obj$sv, 2, var)
-sv.rank <- order(sv.vars, decreasing = TRUE)
-
-vars.df <- data.frame(
-  SV = factor(paste0("SV", 1:ncol(sv.obj$sv)), levels = paste0("SV", 1:ncol(sv.obj$sv))),
-  Variance = sv.vars)
-
-ggplot(vars.df, aes(x = SV, y = Variance)) +
-  geom_bar(stat = "identity") +
-  theme_minimal() +
-  labs(title = "Variance Explained by Surrogate Variables", y = "Variance", x = "Surrogate Variable")
-
-# Checking if the variances are indeed all the same (as seen in the scree plot above)
-options(digits=15)
-apply(sv.obj$sv, 2, var)
+# Export filtered gxData and metadata for use in other scripts
+write.csv(gxData, "Data/gxData_female.csv")
+write.csv(metadata, "Data/metadata_female.csv")
 
 # Participant table -----------------------------------------------------------
 participant_table <- metadata |>
